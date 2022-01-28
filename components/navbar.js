c0a006c20f746a22bfafc97d64a823bf88e21884
Pedro Toribio <pedrom_t@hotmail.com>
2022-01-28 21:21:30 +0100

    [Navbar] Bug solved

--- components/navbar.js
+++ components/navbar.js
@@ -15,6 +15,8 @@ import {
   useColorModeValue
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
+// import ThemeToggleButton from './theme-toggle-button'
+// import { IoLogoGithub } from 'react-icons/io5'
 
 const LinkItem = ({ href, path, children }) => {
   const active = path === href
@@ -59,9 +61,10 @@ const Navbar = props => {
             <Logo />
           </Heading>
         </Flex>
+
         <Stack
           direction={{ base: 'column', md: 'row' }}
-          display={{ base: 'full', md: 'flex' }}
+          display={{ base: 'none', md: 'flex' }}
           width={{ base: 'full', md: 'auto' }}
           alignItems="center"
           flexGrow={1}
@@ -73,32 +76,48 @@ const Navbar = props => {
           <LinkItem href="/posts" path={path}>
             Posts
           </LinkItem>
+          <LinkItem
+            _target="_blank"
+            href="https://github.com/craftzdog/craftzdog-homepage"
+            path={path}
+            display="inline-flex"
+            alignItems="center"
+            style={{ gap: 4 }}
+            pl={2}
+          >
+            {/* <IoLogoGithub /> */}
+            Source
+          </LinkItem>
         </Stack>
 
         <Box flex={1} align="right">
+          {/* <ThemeToggleButton /> */}
+
           <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
-            <Menu>
+            <Menu isLazy id="navbar-menu">
               <MenuButton
                 as={IconButton}
                 icon={<HamburgerIcon />}
                 variant="outline"
                 aria-label="Options"
-              >
-                  <MenuList>
-                      <NextLink href='/' passHref>
-                          <MenuItem as={Link}>About</MenuItem>
-                      </NextLink>
-                      <NextLink href='/works' passHref>
-                          <MenuItem as={Link}>Works</MenuItem>
-                      </NextLink>
-                      <NextLink href='/posts' passHref>
-                          <MenuItem as={Link}>Posts</MenuItem>
-                      </NextLink>
-                      {/* <NextLink href='/posts' passHref>
-                          <MenuItem as={Link} href={Link={}}>Posts</MenuItem>
-                      </NextLink> */}
-                  </MenuList>
-              </MenuButton>
+              />
+              <MenuList>
+                <NextLink href="/" passHref>
+                  <MenuItem as={Link}>About</MenuItem>
+                </NextLink>
+                <NextLink href="/works" passHref>
+                  <MenuItem as={Link}>Works</MenuItem>
+                </NextLink>
+                <NextLink href="/posts" passHref>
+                  <MenuItem as={Link}>Posts</MenuItem>
+                </NextLink>
+                <MenuItem
+                  as={Link}
+                  href="https://github.com/craftzdog/craftzdog-homepage"
+                >
+                  View Source
+                </MenuItem>
+              </MenuList>
             </Menu>
           </Box>
         </Box>
