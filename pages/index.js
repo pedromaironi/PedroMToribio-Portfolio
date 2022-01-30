7d5ac8f276f1c57cec3600c124ee2e062de5e463
Pedro Toribio <pedrom_t@hotmail.com>
2022-01-30 23:02:13 +0100

    [Toggle-theme-Button] Change the theme

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
+import NextLink from 'next/link'
+
 import {
+  Button,
   Container,
   Box,
   Heading,
   Image,
+  Link,
   useColorModeValue
 } from '@chakra-ui/react'
 import Logo from '../components/logo'
 import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
   return (
@@ -53,9 +60,57 @@ const Page = () => {
         <Heading as="h3" variant="section-title">
           Work
         </Heading>
-        <p>A</p>
-
+        <Paragraph>
+          Pedro is a freelance and a front-end developer based in Dominican
+          Republic with a passion for building a web/mobile applications from
+          planning and designing all the way to solving real-life problems with
+          code. When not online, he love play video games. Currently, he is
+          living off of his own product called{' '}
+          <NextLink href="/work/inksales">
+            <Link>inksales</Link>
+          </NextLink>
+          .
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My Portfolio
+            </Button>
+          </NextLink>
+        </Box>
       </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>2000</BioYear>
+          Born in Dominican Republic, Santiago de los Caballeros
+        </BioSection>
+        <BioSection>
+          <BioYear>2021</BioYear>
+          Worked at CC Software Group, S.R.L.
+        </BioSection>
+        <BioSection>
+          <BioYear>2022 to present</BioYear>
+          Work as a
+        </BioSection>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <Paragraph>
+          Music,{' '}
+          <Link href="https://github.com/pedromaironi" target="_blank">
+            Coding
+          </Link>
+          , Play Video Games,{' '}
+        </Paragraph>
+      </Section>
+      
     </Container>
   )
 }
